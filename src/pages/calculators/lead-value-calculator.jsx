0f745d7a9ce18f844cedae72bb318cf3e5e58481
hometownwebco1 <woody@hometownwebco.com>
2025-08-01 14:43:17 -0400

    Update lead-value-calculator.jsx

--- src/pages/calculators/lead-value-calculator.jsx
+++ src/pages/calculators/lead-value-calculator.jsx
@@ -1,2 +1,9 @@
-const LeadValueCalculator = () => <div style={{color:'black',fontSize:'40px'}}>TEST PAGE</div>
+import { useState } from 'react'
+import { Helmet } from 'react-helmet'
+import { Calculator, DollarSign, Users, BarChart2, Mail, TrendingUp } from 'lucide-react'
+import { Link } from 'react-router-dom'
+
+const LeadValueCalculator = () => {
+  return <div style={{color:'black',fontSize:'40px'}}>TEST PAGE</div>
+}
 export default LeadValueCalculator
